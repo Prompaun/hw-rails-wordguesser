cca88274a9bbdc11e61111712be9d65625e4c814
pmyyyy <promp@LAPTOP-69L9RNF3>
2023-08-20 10:40:45 +0000

    complete

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-<<<<<<< HEAD
 Rails.application.initialize!
-=======
-Rails.application.initialize!
->>>>>>> 0c963840d1c35cd9fc2cfa9d54e616f0464e135d
